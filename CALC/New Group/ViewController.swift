3d602b07b77fd44ecf97625074e287b16353b2a6
ket <ketvitketvit@gmail.com>
2018-03-14 22:54:36 +0200

    Version 1.1 1. Repair division by zero. 2. Correction dot  issue.

--- CALC/New Group/ViewController.swift
+++ CALC/New Group/ViewController.swift
@@ -16,8 +16,10 @@ var myOperator = 0
 
 // Beginning. Create class ViewController.
 class ViewController: UIViewController {
+
     // Display.
     @IBOutlet weak var display: UILabel!
+
 
     //  0,1,2,3,4,5,6,7,8,9 buttons.
     @IBAction func myNumbers(_ sender: UIButton) {
@@ -28,6 +30,7 @@ class ViewController: UIViewController {
             secondNumeral = secondNumeral + String(sender.tag) // If press one of the operations button, secondNumeral assign second input value.
         }
     }
+
 
     //  "-", "+", "*", "/", "Power()"  buttons.
     @IBAction func arithmeticOperations(_ sender: UIButton) {
@@ -40,13 +43,24 @@ class ViewController: UIViewController {
                     firstNumeral = String(Double(firstNumeral)! - Double(secondNumeral)!)
                 case 1003:
                     firstNumeral = String(Double(firstNumeral)! * Double(secondNumeral)!)
-                case 1004:
-                    firstNumeral = String(Double(firstNumeral)! / Double(secondNumeral)!)
-                default:
+                case 1004: // Check division by zero.
+                    if Int(Double(secondNumeral)!) != 0 {
+                        firstNumeral = String(Double(firstNumeral)! / Double(secondNumeral)!)
+                    } else {
+                        display.text = "Error";
+                        firstNumeral = "";
+                        secondNumeral = "";
+                        myOperator = 0 }
+                case 1005:
+                    firstNumeral = String(pow(Double(firstNumeral)!, Double(secondNumeral)!))
+            default:
                     print("")
             }
-            // After math we must clear secondNumeral variable, fo other math-operations.
-            secondNumeral = ""
+        // If we have first input value, here we check(compare) to determine, how!!!  output this value on display, with dot(if value have some integers(and not ZERO too) in exponential part), or output integer value, without dot.
+            if Double(firstNumeral) == Double(Int(Double(firstNumeral) ?? 0)) {
+                firstNumeral = String(Int(Double(firstNumeral) ?? 0))
+            }
+            secondNumeral = "" // After math we must clear secondNumeral variable, fo other math-operations.
         }
         // We will get here when we don't have second input value, and after save operator input tag value in myOperator variable, and clear displaye. Each button has its own tag integer value.
         if firstNumeral != "" {
@@ -55,19 +69,19 @@ class ViewController: UIViewController {
         }
     }
 
+
     // "." dot-button.
     @IBAction func decimal(_ sender: Any) {
-        if Int(display.text!) != nil {
-            display.text = display.text! + "."       // If we have input integer Int value we cancatenate dot to this value.
-            if secondNumeral == "" {
-                firstNumeral = firstNumeral + "."    // If we don't have second value, we save first input value to firstNumeral variable.
+        if Int(firstNumeral) != nil && myOperator == 0 {
+            display.text =  display.text! + "."               // If we have input integer Int value we cancatenate dot to this value.
+            firstNumeral = String(Int(firstNumeral)!) + "."   // If we don't have second value, we save first input value to firstNumeral variable.
+        }else if Int(secondNumeral) != nil{
+            display.text = display.text! + "."                // If we have input integer Int value we cancatenate dot to this value.
+            secondNumeral = String(Int(secondNumeral)!) + "." // If we have second input value, we save this in secondNumeral variable.
 
-            }else {
-                secondNumeral = secondNumeral + "."  // If we have second input value, we save this in secondNumeral variable.
-
-            }
         }
     }
+
 
     // "=" button.
     @IBAction func myResult(_ sender: UIButton) {
@@ -80,30 +94,36 @@ class ViewController: UIViewController {
                     firstNumeral = String(Double(firstNumeral)! - Double(secondNumeral)!)
                 case 1003:
                     firstNumeral = String(Double(firstNumeral)! * Double(secondNumeral)!)
-                case 1004:
-                    firstNumeral = String(Double(firstNumeral)! / Double(secondNumeral)!)
+                case 1004: // Check division by zero.
+                    if Int(Double(secondNumeral)!) != 0 {
+                        firstNumeral = String(Double(firstNumeral)! / Double(secondNumeral)!)
+                    } else {
+                        display.text = "Error";
+                        firstNumeral = "";
+                        secondNumeral = "" ;
+                        myOperator = 0}
                 case 1005:
                     firstNumeral = String(pow(Double(firstNumeral)!, Double(secondNumeral)!))
                 default:
                 print("")
+            }
+            // If we have first input value, here we check(compare) to determine, how!!!  output this value on display, with dot(if value have some integers(and not ZERO too) in exponential part), or output integer value, without dot.
+            if Double(firstNumeral) == Double(Int(Double(firstNumeral) ?? 0)) {
+                firstNumeral = String(Int(Double(firstNumeral) ?? 0))
             }
             // After math clear myOperator, secondNumeral variables for other math operations.
             myOperator = 0
             secondNumeral = ""
         }
 
-        // If we have first input value, here we check(compare) to determine, how!!!  output this value on display, with dot(if value have some integers(and not ZERO too) in exponential part), or output integer value, without dot.
         if firstNumeral != "" {
-            if Double(firstNumeral) == Double(Int(Double(firstNumeral) ?? 0)) {
-                display.text = String(Int(Double(firstNumeral) ?? 0))
-            } else {
-                display.text = firstNumeral // We will get here if we have value with dot.
-
-            }
+                display.text = firstNumeral // Print result.
+                myOperator = 0
          }
     }
 
-    // "C/AC (Clear/AllClear)" button.
+    
+        // "C/AC (Clear/AllClear)" button.
     @IBAction func myDelete(_ sender: UIButton) {
         // Clear all variables, operators and clear display.
         firstNumeral = ""
@@ -111,6 +131,13 @@ class ViewController: UIViewController {
         display.text = ""
         myOperator = 0
     }
+
+
+
+
+
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -124,3 +151,12 @@ class ViewController: UIViewController {
 
 }
 // Game Over,  =)
+
+
+
+
+
+
+
+
+
